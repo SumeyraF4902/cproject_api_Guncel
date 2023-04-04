f8278decdd604bb873458b79097c6df3eacaac8a
Teykinay <teykinaycerkezoglu@gmail.com>
2023-04-04 03:09:06 +0300

    a

--- src/test/java/resources/Token.java
+++ src/test/java/resources/Token.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Config {
+public class Token {
 
     public static String BO_token() {
 
