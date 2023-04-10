a887a9c342e6ecd67b9af5f5b43a45788a0919e2
Mehmet ŞİMŞEK <114697510+mehmet20251990@users.noreply.github.com>
2023-04-10 23:47:09 +0300

    Mehmets (#24)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * git stash le değişiklik olduğu görünen dosyalar
    
    * update

--- src/test/java/Page/roleServices/GetRolAppId.java
+++ src/test/java/Page/roleServices/GetRolAppId.java
@@ -2,23 +2,53 @@ package Page.roleServices;
 
 import BaseUrl.BaseURL;
 import io.restassured.response.Response;
-import org.junit.Test;
+import org.testng.annotations.Test;
 import resources.Token;
-
 import static io.restassured.RestAssured.given;
-
 public class GetRolAppId extends BaseURL {
     @Test
-    public void Test1() {
+    public void GetAllAppRolesAPITest() {
         specification.pathParams("applicationPath", "application", "appIdPath", 2, "rolePath", "role");
-        Response response = given().spec(specification).
-                when().
-                header("Authorization",  Token.BO_token()).
+
+        Response response = given().spec(specification).when().header("Authorization",  Token.BO_token()).
+                                    get("/{applicationPath}/{appIdPath}/{rolePath}");
+
+        response.then().assertThat().statusCode(200);
+    }
+    @Test
+    public void GetAllRolesAPITest() {
+        specification.pathParams("rolePath", "role");
+
+        Response response = given().spec(specification).when().header("Authorization",  Token.BO_token()).
+                get("/{rolePath}");
+
+        response.then().assertThat().statusCode(200);
+    }
+    @Test
+    public void GetRoleByIdAPITest() {
+        specification.pathParams("rolePath", "role", "idPath", 1);
+
+        Response response = given().spec(specification).when().header("Authorization",  Token.BO_token()).
+                get("/{rolePath}/{idPath}");
+
+        response.then().assertThat().statusCode(200);
+    }
+    @Test
+    public void GetAllAppRolesNegativeAPITest() {
+        specification.pathParams("applicationPath", "application", "appIdPath", 12345, "rolePath", "role");
+
+        Response response = given().spec(specification).when().header("Authorization",  Token.BO_token()).
                 get("/{applicationPath}/{appIdPath}/{rolePath}");
-        response.then().
 
-                assertThat().
+        response.then().assertThat().statusCode(404);
+    }
+    @Test
+    public void GetRoleByIdNegativeAPITest() {
+        specification.pathParams("rolePath", "role", "idPath", 12345);
 
-                statusCode(200);
+        Response response = given().spec(specification).when().header("Authorization",  Token.BO_token()).
+                get("/{rolePath}/{idPath}");
+
+        response.then().assertThat().statusCode(404);
     }
 }
