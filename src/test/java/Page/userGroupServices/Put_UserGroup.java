6df165476154aa601bf1df9fe2f18850dbc443e9
Simten <114583150+Unwabu@users.noreply.github.com>
2023-04-11 10:35:32 +0300

    Simten (#26)
    * usergroup method classları eklendi + pouser groupjo içerik değişti
    
    * tc08 e kadar eklendi / put methodunda sıkıntı var
    
    * simten last
    
    * son update

--- src/test/java/Page/userGroupServices/Put_UserGroup.java
+++ src/test/java/Page/userGroupServices/Put_UserGroup.java
@@ -29,34 +29,36 @@ public class Put_UserGroup extends BaseURL {
     public void TC_004() {
 
 
-        specification.pathParam("getUserGroupPath", "user-group");
-        Response response = given().spec(specification).when().header("Authorization", Token.BO_token()).get("/{getUserGroupPath}");
-        JsonPath jsonPath = response.jsonPath();
-        List<Map<String, Object>> userGroups = jsonPath.getList("");
-        int otoID = 0;
-        for (Map<String, Object> userGroup : userGroups) {
-            if (userGroup.get("name").toString().contains("Team1s")) {
-                otoID = (Integer) userGroup.get("id");
-                break;
-            }
-        }
-        int putId = otoID;
-        System.out.println(putId);
+       specification.pathParam("getUserGroupPath", "user-group");
+    //   Response response = given().spec(specification).when().header("Authorization", Token.BO_token()).get("/{getUserGroupPath}");
+    //   JsonPath jsonPath = response.jsonPath();
+    //   List<Map<String, Object>> userGroups = jsonPath.getList("");
+    //   int otoID = 0;
+    //   for (Map<String, Object> userGroup : userGroups) {
+    //       if (userGroup.get("name").toString().contains("Team1s")) {
+    //           otoID = (Integer) userGroup.get("id");
+    //           break;
+    //       }
+    //   }
+    //   int putId = otoID;
+    //   System.out.println(putId);
 
-        Group_type gt = new Group_type(1, "Department", "User account is deactivated, and not authorized to access any the application");
+       Group_type gt = new Group_type(1, "Department", "User account is deactivated, and not authorized to access any the application");
 
-        Organization og = new Organization(181, "TestEvolve", 43, "TE", "2023-04-10T13:08:39.699420Z", "2023-04-10T13:08:39.699421Z");
+       Organization og = new Organization(181, "TestEvolve", 43, "TE", "2023-04-10T13:08:39.699420Z", "2023-04-10T13:08:39.699421Z");
 
-        ArrayList<Map<String, Integer>> roles = new ArrayList<>();
-        Map<String, Integer> rolesMap = new HashMap<>();
-        rolesMap.put("id", 5);
-        roles.add(rolesMap);
+       ArrayList<Map<String, Integer>> roles = new ArrayList<>();
+       Map<String, Integer> rolesMap = new HashMap<>();
+       rolesMap.put("id", 5);
+       roles.add(rolesMap);
 
 
 
         UserGroupMainPojo reqBody =
-                new UserGroupMainPojo(putId, "DepartmentTeam1s", 1, gt, 1, og, roles, "deldeldel");
+          //      new UserGroupMainPojo(putId, "DepartmentTeam1s", 1, gt, 1, og,roles ,"denemedeldel");
+                new UserGroupMainPojo(105, "DepartmentTeam1s", 1, gt, 1, og,roles ,"denemedeldel");
         System.out.println(reqBody);
+
         Response responsePut = given().spec(specification).when().contentType(ContentType.JSON).header("Authorization", Token.BO_token()).body(reqBody).put("/{getUserGroupPath}");
         responsePut.prettyPrint();
         //    JsonPath jsonPathForPut = responsePut.jsonPath();
